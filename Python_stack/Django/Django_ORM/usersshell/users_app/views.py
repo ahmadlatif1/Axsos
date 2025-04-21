4a750af3735207859ded22e940e52fab3810ae54
ahmadlatif1 <121526608+ahmadlatif1@users.noreply.github.com>
2025-04-21 14:47:50 +0300

    userstemplate

--- Python_stack/Django/Django_ORM/usersshell/users_app/views.py
+++ Python_stack/Django/Django_ORM/usersshell/users_app/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from users_app.models import *
 
-# Create your views here.
 def index(request):
-    return render(request,"index.html")
+
+
+    users = User.objects.all()
+    return render(request,"index.html", {'users': users})
+
+
+def create_user(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email_address')
+        age = request.POST.get('age')
+        user = User.objects.create(first_name=first_name, last_name=last_name, email_address=email, age=age)
+        user.save()
+    return redirect('/')
