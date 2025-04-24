364df2185d2a9334d982b24980250786805ddcb8
ahmadlatif1 <121526608+ahmadlatif1@users.noreply.github.com>
2025-04-24 16:46:51 +0300

    validation

--- Python_stack/Django/Django_fullstack/restfulshows/app/models.py
+++ Python_stack/Django/Django_fullstack/restfulshows/app/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        if len(postData['description']) < 10:
+            errors['description'] = "Description should be at least 10 characters"
+        if postData['release_date']:
+            release_date = datetime.datetime.strptime(postData['release_date'], "%Y-%m-%d").date()
+            if release_date > datetime.date.today():
+                errors['release_date'] = "Release date should be in the past"
+        if Show.objects.filter(title=postData['title']).exists():
+            errors['title'] = "Title should be unique"
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network= models.CharField(max_length=255)
@@ -8,4 +26,4 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    objects = ShowManager()
--- Python_stack/Django/Django_fullstack/restfulshows/app/views.py
+++ Python_stack/Django/Django_fullstack/restfulshows/app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
 
 def index(request):
@@ -10,14 +10,19 @@ def index(request):
 
 def newshow(request):
     if request.method == "POST":
-        title = request.POST['title']
-        network = request.POST['network']
-        release_date = request.POST['release_date']
-        description = request.POST['description']
-        Show.objects.create(title=title, network=network, release_date=release_date, description=description)
-        return redirect(f"/shows/{Show.objects.last().id}/")
+        errors = Show.objects.validate(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect("/shows/new")
+        else:
+            title = request.POST['title']
+            network = request.POST['network']
+            release_date = request.POST['release_date']
+            description = request.POST['description']
+            Show.objects.create(title=title, network=network, release_date=release_date, description=description)
+            return redirect(f"/shows/{Show.objects.last().id}/")
     return render(request,"addshow.html")
-
 
 def showdetail(request,id):
     context = {
@@ -32,15 +37,23 @@ def editshow(request,id):
     return render(request,"editshow.html",context)
 
 def updateshow(request,id):
-    c = Show.objects.get(id=id)
-    c.title = request.POST['title']
-    c.network = request.POST['network']
-    c.release_date = request.POST['release_date']
-    c.description = request.POST['description']
-    c.save()
-    return redirect(f"/shows/{id}/")
+    if request.method == "POST":
+        errors = Show.objects.validate(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f"/shows/{id}/edit")
+        else:
+            show = Show.objects.get(id=id)
+            show.title = request.POST['title']
+            show.network = request.POST['network']
+            show.release_date = request.POST['release_date']
+            show.description = request.POST['description']
+            show.save()
+            return redirect(f"/shows/{id}/")
+    return redirect(f"/shows/{id}/edit") # Handle GET requests to this URL
 
 def deleteshow(request,id):
-    c = Show.objects.get(id=id)
-    c.delete()
+    show = Show.objects.get(id=id)
+    show.delete()
     return redirect("/shows/")
