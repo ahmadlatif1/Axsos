364209ae1228bf6eb005f3c7b9c287e98dd22668
ahmadlatif1 <121526608+ahmadlatif1@users.noreply.github.com>
2025-05-13 13:44:29 +0300

    edit

--- Java_Stack/Java_fundamentals/first_project.java
+++ Java_Stack/Java_fundamentals/first_project.java
@@ -2,8 +2,8 @@ package Java_fundamentals;
 
 public class first_project {
     public static void main(String[] args) {
-        System.out.println("My name is Axsos Academy");
-        System.out.println("I am 5 years old");
-        System.out.println("My Hometowm is Ramallah");
+        System.out.println("My name is Ahmad Abdellatif");
+        System.out.println("I am 21 years old");
+        System.out.println("My Hometowm is Jerusalem");
     }
 }
