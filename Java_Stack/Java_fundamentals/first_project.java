e18d81da8cfa06533838e0fe912b0f47c5eb7bf7
ahmadlatif1 <121526608+ahmadlatif1@users.noreply.github.com>
2025-05-13 13:41:41 +0300

    first java commit

---
+++ Java_Stack/Java_fundamentals/first_project.java
@@ -0,0 +1,9 @@
+package Java_fundamentals;
+
+public class first_project {
+    public static void main(String[] args) {
+        System.out.println("My name is Axsos Academy");
+        System.out.println("I am 5 years old");
+        System.out.println("My Hometowm is Ramallah");
+    }
+}
