4f6cb4b95b5f8953ef182a87cb0b193c286b81ac
ahmadlatif1 <121526608+ahmadlatif1@users.noreply.github.com>
2025-07-01 14:35:00 +0300

    a

--- MERN_Stack/javascript_OOP/dojoCCG/Card.js
+++ MERN_Stack/javascript_OOP/dojoCCG/Card.js
@@ -17,7 +17,7 @@ class Unit extends Card{
     attack(target){
         if(!(target instanceof Unit))throw new Error("Target must be a unit!")
 
-        console.log(this.name+" attacks "+target.name+" with "+this.power+" power!")
+        console.log("\n"+this.name+" attacks "+target.name+" with "+this.power+" power!")
         console.log("before: resilience:"+target.resilience+", power: "+target.power)
         target.resilience-=this.power;
         console.log("after: resilience:"+target.resilience+", power: "+target.power)
@@ -49,7 +49,7 @@ class Effect extends Card{
     play(target) {
         
         if(!(target instanceof Unit))throw new Error("Target must be a unit!")
-        console.log("applied "+this.name+" on "+target.name+"!")
+        console.log("\napplied "+this.name+" on "+target.name+"!")
         console.log("before: resilience:"+target.resilience+", power: "+target.power)
         target.resilience+=this.resilienceEffect;
         target.power+=this.powerEffect;
@@ -59,17 +59,20 @@ class Effect extends Card{
 
 }
 
-orc=new Unit("orc",1,5,4)
-trebuchet=new Unit("trebuchet",1,2,12)
-goblin=new Unit("goblin",1,2,2)
-
-resbuff=new Effect("resilience buff",1,1)
-powerbuff=new Effect("power buff",1,0,1)
-
-poison=new Effect("poison debuff",1,-1)
-weakness=new Effect("weakening debuff",1,0,-1)
+redninja=new Unit("Red belt ninja",3,4,3)
+blackninja=new Unit("Black belt ninja",4,4,5)
 
 
-resbuff.play(trebuchet)
+hardalgo=new Effect("Hard Algorithm",2,3)
+promisereject=new Effect("Unhandled Promise Rejection",1,-2)
+pairprogram=new Effect("Pair Programming",3,0,2)
 
-goblin.attack(trebuchet)
+
+
+hardalgo.play(redninja)
+
+promisereject.play(redninja)
+
+pairprogram.play(redninja)
+
+redninja.attack(blackninja)
