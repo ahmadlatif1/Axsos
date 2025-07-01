4be809c68fcf8bad5a42e563ad0a05070275b36f
ahmadlatif1 <121526608+ahmadlatif1@users.noreply.github.com>
2025-07-01 13:12:26 +0300

    super

--- MERN_Stack/javascript_OOP/ninja.js
+++ MERN_Stack/javascript_OOP/ninja.js
@@ -8,18 +8,43 @@ class Ninja{
         this.health=health
     }
 
-     sayName=()=> {
+    sayName=()=> {
         console.log(this.name)
     }
-     showStats=()=> {
+    showStats=()=> {
         console.log(this)
     }
     
-     drinkSake=()=> {
+    drinkSake=()=> {
         if(health+10>100)
             health=100;
-        else health++
+        else health+=10
     }
+
+}
+
+class Sensei extends Ninja{
+
+    constructor(name,strength=10,speed=10,health=200,wisdom=10){
+        this.name=name
+        this.speed=speed
+        this.strength=strength
+        this.health=health
+        this.wisdom=10
+    }
+
+    drinkSake=()=> {
+        if(health+10>200)
+            health=200;
+        else health+=10
+    }
+
+    speakWisdom=()=> {
+        this.drinkSake()
+        console.log("i think hamburger goes well with fries")
+        this.wisdom++
+    }
+
 
 }
 
